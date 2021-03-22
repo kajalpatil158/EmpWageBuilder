835abf451f0109934e4b0a941a69aaa558cfa1c6
Kajal <kajalpatil158@gmail.com>
2021-03-22 14:04:21 +0530

    [Kajal Patil]Added: Part Time Employee

--- EmployeeWageBuilder/src/empwagebuilder/EmpWageBuilderUC2.java
+++ EmployeeWageBuilder/src/empwagebuilder/EmpWageBuilderUC2.java
@@ -1,25 +1,32 @@
 package empwagebuilder;
 
-public class EmpWageBuilderUC2 
+public class EmpWageBuilderUC2
 {
 	public static void main(String[] args)	
 	{
 		//Constants
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int Wage_Per_Hr=20;
 		//Veriables
 		int emp_hr;
 		//Computation
-		double empCheck = Math.floor(Math.random()*10)%2;
+		double empCheck = (int)Math.floor(Math.random()*10);
 		if (empCheck == IS_FULL_TIME)
 		{
 			emp_hr=8;
 			System.out.println("Employee is Present");
 		}
+		else if(empCheck == IS_PART_TIME)
+		{
+			emp_hr=4;
+			System.out.println("Employee is Part Time");
+		}
 		else
 		{
 			emp_hr=0;
 			System.out.println("Employee is Absent");
+			
 		}
 		int Wage = (emp_hr) * (Wage_Per_Hr);
 		System.out.println("Employee wages per hr is = "+Wage);
