2508cafe2aa461b0fac8172a84381f94271b2e56
Kajal <kajalpatil158@gmail.com>
2021-03-23 10:54:45 +0530

    [Kajal Patil]Added: Days and Hours Wages

--- EmployeeWageBuilder/src/empwagebuilder/EmpWageBuilderUC.java
+++ EmployeeWageBuilder/src/empwagebuilder/EmpWageBuilderUC.java
@@ -1,40 +1,40 @@
 package empwagebuilder;
+
 import java.util.Random;
-public class EmpWageBuilderUC
-{
-	public static void main(String[] args)	
-	{
-		//Constants
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int Wage_Per_Hr=20;
-		int WORKING_DAYS_FOR_MONTH=20;
-		//Veriables
-		int emp_hr;
-		//Computation
-		Random rand=new Random();
-		int empCheck =rand.nextInt(3);
-		switch (empCheck)
-		{
-		case  1:
-				emp_hr=8;
-				System.out.println("Employee is Present");
+
+public class EmpWageBuilderUC {
+	// Constants
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int Emp_Rate_Per_Hr = 20;
+	public static final int WORKING_DAYS_FOR_MONTH = 20;
+	public static final int TOTAL_WORKING_HR = 10;
+
+	public static void main(String[] args) {
+		// Veriables
+		int empHr = 0;
+		int totalEmpHr = 0;
+		int totalWorkingDays = 0;
+		// Computation
+		while (totalEmpHr <= TOTAL_WORKING_HR && totalWorkingDays < WORKING_DAYS_FOR_MONTH) {
+			totalWorkingDays++;
+			Random rand = new Random();
+			int empCheck = rand.nextInt(3);
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHr = 8;
 				break;
-		case 2:
-				emp_hr=4;
-				System.out.println("Employee is Part Time");
+			case IS_PART_TIME:
+				empHr = 4;
 				break;
-		default:
-				emp_hr=0;
-				System.out.println("Employee is Absent");
+			default:
+				empHr = 0;
 				break;
+			}
+			totalEmpHr += empHr;
+			System.out.println("Days" + totalWorkingDays + " Emplyee Hours is =" + empHr);
 		}
-		int Wage = (emp_hr) * (Wage_Per_Hr);
-		int Total_Wages=(emp_hr) * (Wage_Per_Hr)*(WORKING_DAYS_FOR_MONTH);
-		System.out.println("Employee wages per hr is = "+Wage);
-		System.out.println("Employee wages for month is = "+Total_Wages);
-		
+		int Total_Emp_Wages = totalEmpHr * Emp_Rate_Per_Hr;
+		System.out.println("Employee wages for month is = " + Total_Emp_Wages);
 	}
-
-
 }
